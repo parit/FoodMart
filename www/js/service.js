2aa75b181bbabc97e3ba7bc10dcc65b5a77ad6d3
Parit Bansal <parit.bansal@isb-sib.ch>
2016-09-17 15:34:27 +0200

    updated service.js

--- www/js/service.js
+++ www/js/service.js
@@ -28,3 +28,57 @@ function getData(content) {
                 console.log(data);
             });
 }
+
+function processDescriptions(response) 
+{
+    var alreadyConsideredIndexes = [];
+    var descriptions = [];
+    var textAnnotations = responses[0].textAnnotations; 
+
+    for (var i = 0; i < textAnnotations.length; i++) {
+        var description = [textAnnotations.description];
+        for (var j = i + 1; j < textAnnotations.length; j++) {
+            if (alreadyConsideredIndexes.indexOf(j) == -1) 
+            {
+                var diff = Math.abs(textAnnotations[i].boundingPoly.vertices[0].y);
+                if (diff < 4) 
+                {
+                    description.push(textAnnotations[j].description);
+                    alreadyConsideredIndexes.push(j);
+                }
+            }
+        }
+        descriptions.push(description.join(' '));
+    }
+    return descriptions;
+}
+
+function getFoods(descriptions, callback) {
+    // call api and filter foods
+    
+}
+
+// call this method after taking the picture
+function process(content) {
+    // take picture and call this function;
+    getData(content, function(data){
+        // call api to get descriptions processDescriptions
+        var descriptions = processDescriptions(data);
+        // call api to get foods getFoods
+        getFoods(descriptions, function(data){
+            var foods = [];
+            foods = foods.map(function(food){
+                var expiringOn = (new Date()).setDate((new Date()).date() + food.days);
+                return { expiringOn: (new Date()).toLocaleDateString(), description: food};
+            });
+
+            // add these foods to the store addFoodToTheStore
+
+            // call render based on whats in the store
+        });
+    });
+}
+
+function render(foods) {
+    foods
+}
